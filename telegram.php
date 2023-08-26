561989c127666da5916cb922e08200f32f05c648
n-epanesh <125089283+n-epanesh@users.noreply.github.com>
2023-08-26 16:51:39 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -3,10 +3,10 @@
 // https://api.telegram.org/bot5994251403:AAEky1sf_-Jbl6u-ApMVxyQl-TGPa6Nym5M/sendMessage?chat_id=-1001925081180&text=
 
 //В переменную $token нужно вставить токен, который нам прислал @botFather
-$token = "5994251403:AAEky1sf_-Jbl6u-ApMVxyQl-TGPa6Nym5M";
+$token = "1384146931:AAHvW0vB0IFsp61M9IDsuv0Wxq2Gq4keEho";
 
 //Сюда вставляем chat_id
-$chat_id = "-1001925081180";
+$chat_id = "-1001975167698";
 
 //Определяем переменные для передачи данных из нашей формы
 if ($_POST['act'] == 'order') {
